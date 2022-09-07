7162c0b7d0c432529a1deb3e439916f34d87b8e0
NamanRawal0471 <naman0471.be20@chitkara.edu.in>
2022-09-07 14:11:39 +0530

    Added Recommended Courses

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Component } from 'react';
 import './App.css';
 import Hero from './Hero/hero.jsx'
+import Recommended_Courses from './Recommended_Courses/Recommended_Courses';
 
 class App extends Component{
   render(){
   return (
     <div className='container'>
       <Hero />
+      <Recommended_Courses/>
     </div>
   );
 }
---
+++ src/Recommended_Courses/Recommended_Courses.js
@@ -0,0 +1,158 @@
+import './Recommended_Course.css';
+
+function Recommended_Courses() {
+    return (
+      <div className="App">
+        <br/><br/>
+      <div id="carouselExampleControls" className="carousel carousel-dark slide" data-bs-touch="false">
+          
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <div className="card-holder" >
+              <div className="card" >
+                  <img src="https://source.unsplash.com/50x50/?laptop" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title">Course 1</h5>
+                    <p className="card-text">Instructor's Name</p>
+                    <p className="card-text">Rating: </p>
+                    <p className="card-text">Price:</p>
+                    <a href="#" className="btn btn-primary">Go Somewhere</a>
+                  </div>
+                </div>
+                <div className="card" >
+                  <img src="https://source.unsplash.com/50x50/?laptop" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title">Course 2</h5>
+                    <p className="card-text">Instructor's Name</p>
+                    <p className="card-text">Rating: </p>
+                    <p className="card-text">Price:</p>
+                    <a href="#" className="btn btn-primary">Go Somewhere</a>
+                  </div>
+                </div>
+                <div className="card" >
+                  <img src="https://source.unsplash.com/50x50/?laptop" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title">Course 3</h5>
+                    <p className="card-text">Instructor's Name</p>
+                    <p className="card-text">Rating: </p>
+                    <p className="card-text">Price:</p>
+                    <a href="#" className="btn btn-primary">Go Somewhere</a>
+                  </div>
+                </div>
+                <div className="card"  id="responsiveCard">
+                  <img src="https://source.unsplash.com/50x50/?laptop" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title">Course 4</h5>
+                    <p className="card-text">Instructor's Name</p>
+                    <p className="card-text">Rating: </p>
+                    <p className="card-text">Price:</p>
+                    <a href="#" className="btn btn-primary">Go Somewhere</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <div className="card-holder">
+                  <div className="card" >
+                      <img src="https://source.unsplash.com/50x50/?laptop" className="card-img-top" alt="..."/>
+                      <div className="card-body">
+                        <h5 className="card-title">Course 6</h5>
+                        <p className="card-text">Instructor's Name</p>
+                        <p className="card-text">Rating: </p>
+                    <p className="card-text">Price:</p>
+                    <a href="#" className="btn btn-primary">Go Somewhere</a>
+                      </div>
+                    </div>
+                    <div className="card" >
+                      <img src="https://source.unsplash.com/50x50/?laptop" className="card-img-top" alt="..."/>
+                      <div className="card-body">
+                        <h5 className="card-title">Course 7</h5>
+                        <p className="card-text">Instructor's Name</p>
+                        <p className="card-text">Rating: </p>
+                    <p className="card-text">Price:</p>
+                    <a href="#" className="btn btn-primary">Go Somewhere</a>
+                      </div>
+                    </div>
+                    <div className="card" >
+                      <img src="https://source.unsplash.com/50x50/?laptop" className="card-img-top" alt="..."/>
+                      <div className="card-body">
+                        <h5 className="card-title">Course 8</h5>
+                        <p className="card-text">Instructor's Name</p>
+                        <p className="card-text">Rating: </p>
+                    <p className="card-text">Price:</p>
+                    <a href="#" className="btn btn-primary">Go Somewhere</a>
+                      </div>
+                    </div>
+                    <div className="card" id="responsiveCard1">
+                      <img src="https://source.unsplash.com/50x50/?laptop" className="card-img-top" alt="..."/>
+                      <div className="card-body">
+                        <h5 className="card-title" >Course 9</h5>
+                        <p className="card-text">Instructor's Name</p>
+                        <p className="card-text">Rating: </p>
+                    <p className="card-text">Price:</p>
+                    <a href="#" className="btn btn-primary">Go Somewhere</a>
+                      </div>
+                    </div>
+                  </div>
+            </div>
+            <div className="carousel-item" >
+              <div className="card-holder" >
+                  <div className="card" >
+                      <img src="https://source.unsplash.com/50x50/?laptop" className="card-img-top" alt="..."/>
+                      <div className="card-body">
+                        <h5 className="card-title">Course 10</h5>
+                        <p className="card-text">Instructor's Name</p>
+                        <p className="card-text">Rating: </p>
+                    <p className="card-text">Price:</p>
+                    <a href="#" className="btn btn-primary">Go Somewhere</a>
+                      </div>
+                    </div>
+                    <div className="card" >
+                      <img src="https://source.unsplash.com/50x50/?laptop" className="card-img-top" alt="..."/>
+                      <div className="card-body">
+                        <h5 className="card-title">Course 11</h5>
+                        <p className="card-text">Instructor's Name</p>
+                        <p className="card-text">Rating: </p>
+                    <p className="card-text">Price:</p>
+                    <a href="#" className="btn btn-primary">Go Somewhere</a>
+                      </div>
+                    </div>
+                    <div className="card" >
+                      <img src="https://source.unsplash.com/50x50/?laptop" className="card-img-top" alt="..."/>
+                      <div className="card-body">
+                        <h5 className="card-title">Course 12</h5>
+                        <p className="card-text">Instructor's Name</p>
+                        <p className="card-text">Rating: </p>
+                    <p className="card-text">Price:</p>
+                    <a href="#" className="btn btn-primary">Go Somewhere</a>
+                      </div>
+                    </div>
+                    <div className="card" id="responsiveCard2">
+                      <img src="https://source.unsplash.com/50x50/?laptop" className="card-img-top" alt="..."/>
+                      <div className="card-body">
+                        <h5 className="card-title" >Course 13</h5>
+                        <p className="card-text">Instructor's Name</p>
+                        <p className="card-text">Rating: </p>
+                    <p className="card-text">Price:</p>
+                    <a href="#" className="btn btn-primary">Go Somewhere</a>
+                      </div>
+                    </div>
+                  </div>
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev" >
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+        
+    );
+  }
+  
+  export default Recommended_Courses;
+  

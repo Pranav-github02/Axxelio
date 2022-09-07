3453a112326e6b0a75d1f869efbc5c725b91ca1d
mohit0234 <mohit0464.be20@chitkara.edu.in>
2022-09-07 13:26:23 +0530

    added hero component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import Hero from './Hero/hero.jsx'
 
-function App() {
+class App extends Component{
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Hero />
     </div>
   );
 }
+}
 
 export default App;
+
+
---
+++ src/Hero/hero.jsx
@@ -0,0 +1,23 @@
+import './hero.css'
+import naman from './naman.jpeg'
+
+const Hero = () => {
+    return (
+        <>
+            <div className="hero-section">
+                <div className="hero-section-content">
+                    <h1 className="text">Learn Without Limits</h1>
+                    <p className="pac">Start,switch,or advance your career with more than 5,000
+                        courses,Professional certificates from world-class
+                        instructors.
+                    </p>
+                </div>
+                <div className="hero-img">
+                    <img id="img" src={naman} alt="rg" />
+                </div>
+            </div>
+
+        </>
+    )
+}
+export default Hero;
